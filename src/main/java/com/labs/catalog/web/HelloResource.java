0b19de5b535d6c758db73d5b1730f75f5bd24cb8
hadipsy27 <hadipsy27@gmail.com>
2023-09-07 22:14:47 +0700

    Logging

--- src/main/java/com/labs/catalog/web/HelloResource.java
+++ src/main/java/com/labs/catalog/web/HelloResource.java
@@ -1,12 +1,15 @@
 package com.labs.catalog.web;
 
 import com.labs.catalog.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
 
+    Logger logger = LoggerFactory.getLogger(HelloResource.class);
     private GreetingService greetingService;
 
     public HelloResource(GreetingService greetingService) {
@@ -15,6 +18,11 @@ public class HelloResource {
 
     @GetMapping("/hello")
     public String helloWorld(){
+        logger.trace("This is Log TRACE");
+        logger.debug("This is Log DEBUG");
+        logger.info("This is Log INFO");
+        logger.warn("This is Log WARN");
+        logger.error("This is Log ERROR");
         return greetingService.sayGreetings();
     }
 }
