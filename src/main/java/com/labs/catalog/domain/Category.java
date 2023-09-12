f7c253b5e3f396c100bb79c6aae3b9763b2358be
hadipsy27 <hadipsy27@gmail.com>
2023-09-12 22:01:27 +0700

    JPA = Many to Many Relationship: Book - Category

---
+++ src/main/java/com/labs/catalog/domain/Category.java
@@ -0,0 +1,31 @@
+package com.labs.catalog.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7370220842171704593L;
+
+    @Id
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    // Many to many relationships yang bersifat unidirectional
+    @ManyToMany(mappedBy = "categories")
+    private List<Book> books;
+}
