669dd08de84f5a0ee3854fce0e391f9dd5ebf6e6
hadipsy27 <hadipsy27@gmail.com>
2023-09-18 21:34:16 +0700

    Implement Spring AOP - Create Log if excute service book detail

--- src/main/java/com/labs/catalog/BookCatalogApplication.java
+++ src/main/java/com/labs/catalog/BookCatalogApplication.java
@@ -2,7 +2,9 @@ package com.labs.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class BookCatalogApplication {
 
---
+++ src/main/java/com/labs/catalog/aspect/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.labs.catalog.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Aspect
+public class LoggingAspect {
+
+    @Pointcut("execution(* com.labs.catalog.web.BookResource.findBookDetail(..))")
+    public void restAPI(){}
+
+    @Before("restAPI()")
+    public void beforeExecutedLogging(){
+        log.info("This is log from aspect");
+    }
+}
