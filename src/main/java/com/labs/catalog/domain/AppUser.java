de31a298c4821c97618fecf812c43ac49066460b
hadipsy27 <hadipsy27@gmail.com>
2023-09-21 09:38:29 +0700

    Menambahkan Entity User untuk kebutuhan login dan register

---
+++ src/main/java/com/labs/catalog/domain/AppUser.java
@@ -0,0 +1,52 @@
+package com.labs.catalog.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class AppUser extends AbstractBaseEntity implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = -7486591533290799884L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
